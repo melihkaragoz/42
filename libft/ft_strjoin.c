ef700fe5d2f6b3fcd7c642d15c3f210d57610c3c
Melih Karagöz <mkaragoz@Melihs-MacBook-Pro.local>
2022-10-18 07:28:23 +0300

    added ft_strjoin

---
+++ libft/ft_strjoin.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkaragoz <mkaragoz@student.42istanbul.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/18 06:57:23 by mkaragoz          #+#    #+#             */
+/*   Updated: 2022/10/18 07:28:00 by mkaragoz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	l1;
+	size_t	l2;
+	char	*n;
+	int		i;
+
+	i = 0;
+	l1 = ft_strlen(s1);
+	l2 = ft_strlen(s2);
+	if (l1 != 0 && l2 != 0)
+	{
+		n = malloc((l1 + l2) + 1 * sizeof(char));
+		if (!n)
+			return (0);
+		while (*s1 != 0)
+			n[i++] = *(s1++);
+		while (*s2 != 0)
+			n[i++] = *(s2++);
+	}
+	n[i] = 0;
+	return (n);
+}
