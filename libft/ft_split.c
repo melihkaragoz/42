fd60af2d5e72abe6dd4356b1ab2fbacd916056fc
Melih Karagöz <mkaragoz@Melihs-MacBook-Pro.local>
2022-10-19 19:55:11 +0300

    ft_split updated

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,55 +6,49 @@
 /*   By: mkaragoz <mkaragoz@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 15:16:50 by mkaragoz          #+#    #+#             */
-/*   Updated: 2022/10/19 17:53:12 by mkaragoz         ###   ########.fr       */
+/*   Updated: 2022/10/19 19:53:17 by mkaragoz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	**ft_split(char const *s, char c)
+char **ft_split(char const *s, char c)
 {
-    int		i;
-    char	*tcp;
-    char	*ns;
-    char	**fp;
-    int		fcp;
-    int		tcpFlag;
+    char **bp;
+    char *n;
+    int    i;
+    int    count;
+    int    j;
 
-    fcp = 0;
-    i = -1;
-    fp = malloc(5 * sizeof(char *));
-    tcpFlag = 0;
-    ns = (char *)s;
-	while (*(ns + (++i)))
-	{
-		if (*(ns + i) != c)
-		{
-			if (!tcpFlag)
-			{
-				tcp = (char *)(ns + i);
-				tcpFlag = 1;
-			}
-		}
-		else
-		{
-			fp[fcp++] = tcp;
-			tcpFlag = 0;
-			ns[i] = 0;
-		}
-	}
-	fp[fcp++] = tcp;
-	*(fp + fcp) = 0;
-	return (fp);
+    i = 0;
+    n = (char *)s;
+    bp = malloc(100 * sizeof(char *));
+    count = 0;
+    while (*(n + i))
+    {
+        if (*(n + i) == c)
+            i++;
+        else
+        {
+            j = i;
+            while (*(n + i) && *(n + i) != c)
+                i++;
+            if (++count && *(n+i))
+              n[i++] = 0;
+            bp[count-1] = (n+j);
+        }
+    }
+    bp[++count] = 0;
+    return (bp);
 }
 
 int    main(void)
 {
-    char    s[] = "melih,karagoz";
+    char    s[] = ",,,,melih,karagoz,42";
     char    sep = ',';
     char **fp = ft_split(s,sep);
     int i = 0;
-    while (i < 2)
+    while (i < 3)
     {
         printf("%s\n",(*(fp+i)));
         i++;
