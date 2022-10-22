fe5b68264a809d210759e3b47194d7b590f9a443
melihkaragoz <melihkkaragoz@hotmail.com>
2022-10-22 20:54:59 +0300

    added ft_strtrim and fixed some functions

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: mkaragoz <mkaragoz@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 22:30:21 by mkaragoz          #+#    #+#             */
-/*   Updated: 2022/10/19 15:16:07 by mkaragoz         ###   ########.fr       */
+/*   Updated: 2022/10/22 20:53:53 by mkaragoz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,21 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	int		fi;
-	int		li;
 	int		i;
+	int		j;
+	char	*ns;
 
+	i = 0;
 	if (*set != 0 && *s1 != 0)
 	{
-		while (s1)
-		{
-
-		}
+		while (s1[i] && ft_strchr(set, s1[i]))
+			i++;
+		j = ft_strlen(s1) - 1;
+		while (s1[j] && ft_strchr(set, s1[j]))
+			j--;
+		ns = ft_substr(s1 + i, 0, (++j - i));
+		if (!ns)
+			return (0);
 	}
+	return (ns);
 }
