4c9f488b549c495854e80f687780a99a438f9695
melihkaragoz <melihkkaragoz@hotmail.com>
2022-10-23 17:39:59 +0300

    added allocation check in ft_strmapi.c

--- libft/ft_striteri.c
+++ libft/ft_striteri.c
@@ -1,33 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mkaragoz <mkaragoz@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/23 17:10:01 by mkaragoz          #+#    #+#             */
-/*   Updated: 2022/10/23 17:39:11 by mkaragoz         ###   ########.fr       */
+/*   Created: 2022/10/23 17:34:39 by mkaragoz          #+#    #+#             */
+/*   Updated: 2022/10/23 17:34:59 by mkaragoz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
-	char			*new;
-	unsigned int	i;
-	unsigned int	len;
-
-	i = 0;
-	if (!(s && f))
-		return (0);
-	len = ft_strlen();
-	new = malloc(len * sizeof(char) + 1);
-	if (!new)
-		return (0);
-	while (s[i])
-	{
-		new[i] = f(i, s[i]);
-		i++;
-	}
-	new[len] = 0;
-	return (new);
 }
